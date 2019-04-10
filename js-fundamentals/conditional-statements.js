bf030865ef0d43262dcdf24a824bde33a02f0ceb
Louie Williford <dustyfingers01257@gmail.com>
2019-04-10 04:35:04 -0600

    culling the herd and continuing to learn fundamentals

--- js-fundamentals/conditional-statements.js
+++
@@ -1,2 +0,0 @@
-console.log((5 > 4) ? 'yes' : 'no');
-console.log((5 > 4444) ? 'yes' : 'no');
---
+++ node-getting-started/destructuring.js
@@ -0,0 +1,9 @@
+// // this is a long way of doing this
+// //
+// const PI = Math.PI;
+// const E = Math.E;
+// const SQRT2 = Math.SQRT2;
+
+// this is the destructuring way of doing this
+//
+const { PI, E, SQRT2 } = Math;
